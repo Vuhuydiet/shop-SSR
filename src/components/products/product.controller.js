9ed617b2de314842ce265495ddcbe304c7fb1ea7
vuhuydiet <Truongvu04072004@gmail.com>
2024-11-05 13:22:31 +0700

    mvc

---
+++ src/components/products/product.controller.js
@@ -0,0 +1,7 @@
+export default {
+
+  listAllProducts: async (req, res) => {
+  },
+
+
+}
--- src/components/products/product.route.js
+++ src/components/products/product.route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const productController = require('./product.controller');
 
+router.use('/products', productController.listAllProducts);
 
 module.exports = router;
