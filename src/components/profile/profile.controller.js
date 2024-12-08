709996c30e683df9a1db8971d11acb8f0ebc6b10
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2024-12-08 20:27:11 +0700

    Add user address page

--- src/components/profile/profile.controller.js
+++ src/components/profile/profile.controller.js
@@ -1,8 +1,26 @@
 module.exports = {
   getUserProfile: (req, res) => {
-    console.log(req.user);
     res.render("pages/profile", {
       user: req.user,
+    });
+  },
+
+  getUserAddress: (req, res) => {
+    const addresses = [
+      {
+        addressId: 1,
+        userId: 1,
+        country: "Vietnam",
+        city: "Ho Chi Minh",
+        district: "Tan Binh",
+        ward: "15",
+        phoneNumber: "0123456789",
+        detailAddress: "123 Nguyen Van Linh",
+        recipientName: "John Doe",
+      },
+    ];
+    res.render("pages/profileAddress", {
+      addresses: addresses,
     });
   },
 };
--- src/components/profile/profile.route.js
+++ src/components/profile/profile.route.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('./profile.controller');
-const { isAuthenticated } = require('../accounts/account.middleware');
+const userController = require("./profile.controller");
+const { isAuthenticated } = require("../accounts/account.middleware");
 
-
-router.get('/', isAuthenticated, userController.getUserProfile);
+router.get("/", isAuthenticated, userController.getUserProfile);
+router.get("/address", isAuthenticated, userController.getUserAddress);
 
 module.exports = router;
