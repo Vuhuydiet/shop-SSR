e310e6eb75e3a5e3a7ac9c9926da2e045814f204
vuhuydiet <Truongvu04072004@gmail.com>
2024-12-12 15:57:31 +0700

    add createSession script

---
+++ src/public/scripts/session.sql
@@ -0,0 +1,11 @@
+-- Active: 1732951377524@@127.0.0.1@5432@shop_ssr_db@public
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
