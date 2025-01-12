bb2f8148a987ccd3ccdd032b0320fb7c267b21fb
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2025-01-12 21:13:00 +0700

    Remove redundant route

--- src/components/orders/order.route.js
+++ src/components/orders/order.route.js
@@ -27,29 +27,6 @@ const queryValidator = () => {
   ];
 };
 
-const createOrderValidator = () => {
-  return [
-    body("phoneNumber").optional().isMobilePhone(),
-    body("country").notEmpty().isString(),
-    body("city").notEmpty().isString(),
-    body("district").notEmpty().isString(),
-    body("ward").notEmpty().isString(),
-    body("addressDetail").optional().isString(),
-    body("paymentMethod").isIn(["COD", "VNPAY"]),
-    body("details").isArray(),
-    body("details.*.productId").isInt(),
-    body("details.*.quantity")
-      .isNumeric()
-      .toInt()
-      .custom((value) => {
-        if (value < 1) {
-          throw new Error("Quantity must be at least 1");
-        }
-        return true;
-      }),
-  ];
-};
-
 router.get(
   "/",
   queryValidator(),
