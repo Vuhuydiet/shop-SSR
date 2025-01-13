5c202dbc7ddc263d28a6e83950c872e5f5be93d6
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2025-01-13 15:24:34 +0700

    Add about page

--- src/components/home/home.route.js
+++ src/components/home/home.route.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const controller = require('./home.controller');
+const controller = require("./home.controller");
 
-router.get('/', controller.getHomePage);
-
+router.get("/", controller.getHomePage);
+router.get("/about", controller.getAboutPage);
+// router.get('/contact', controller.getContactPage);.
 
 module.exports = router;
